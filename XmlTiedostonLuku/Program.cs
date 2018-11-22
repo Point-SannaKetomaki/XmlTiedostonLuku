63ef0c4873e18800e4766cdcd683885087e6c8cc
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2018-11-22 16:17:36 +0200

    Itse luotu XML-tiedoston luku-koodi

--- XmlTiedostonLuku/Program.cs
+++ XmlTiedostonLuku/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace XmlTiedostonLuku
 {
@@ -10,6 +12,24 @@ namespace XmlTiedostonLuku
     {
         static void Main(string[] args)
         {
+            
+            //Luodaan uusi XML-olio
+            XmlDocument myXML = new XmlDocument();
+
+            //Ladataan tiedot ko. polku\tiedosto :sta
+            myXML.Load(@"C:\Users\OWNER\Google Drive\PointCollege\Kurssi_05_Ohjelmoinnin jatkokurssi\menu.xml");
+
+            //Tulostetaan tiedot ruudulle
+            // Tähti (*) tuo ruudulle kaikki tiedot ("name":n sijasta)
+            // Ensin xml -koodin sitten tekstisisällön "selväkielisenä"
+
+            XmlNodeList list = myXML.GetElementsByTagName("name");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i].InnerXml);
+            }
+            Console.ReadLine();
+
         }
     }
 }
